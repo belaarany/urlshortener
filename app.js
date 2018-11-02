55de45468e1a4d943295af332ea664c500e8ce67
Bela Arany <bela.arany@goabela.com>
2018-11-02 14:00:03 +0100

    ENV file validation

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env nodejs
 
 
+// Checking if the ENV file exists
+const fs = require("fs");
+if (fs.existsSync("./.env") === false) {
+    
+    console.error("The ENV file does not exist! Shutting down...")
+
+    process.exit()
+}
+
+
 // Importing the booting file
 require("./boot.js")
