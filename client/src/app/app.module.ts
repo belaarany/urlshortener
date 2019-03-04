00f64e67943ce73e97becdcb65315aec78663197
Bela Arany <bela.arany@goabela.com>
2019-03-04 14:28:48 +0100

    Frontend updates

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header.component';
 import { FooterComponent } from './layout/footer.component';
+import { EditorComponent } from './views/home//editor/editor.component';
+import { HomeComponent } from './views/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    EditorComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule
